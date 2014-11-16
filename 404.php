005268851d180792dd52374236589c2fae8e9344
miyachake <driclogy@gmail.com>
2014-11-16 22:28:48 +0900

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div id="content">
+<h1 class="ta-center"><img src="<?php echo get_template_directory_uri(); ?>/images/img_404.png" alt="404 NotFound." width="509" height="286" /></h1> 
+
+<p class="ta-center fs13">お探しのページは見つかりません。<br />
+一時的にアクセスできない状態か、移動、もしくは削除された可能性があります。</p>
+
+<p class="ta-center fs13"><a href="<?php echo home_url(); ?>">トップページへ戻る</a></p>
+
+</div>
+<!--/content-->
+
+
+
+<?php get_footer(); ?>
